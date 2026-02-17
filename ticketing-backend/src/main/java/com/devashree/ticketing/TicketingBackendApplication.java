7ae659add8e7d1bbb6cfa5f0e468c1eb54035f70
devashree2006 <devashreebjadhav@gmail.com>
2026-02-17 09:56:14 +0530

    Spring Boot backend setup TestController created

---
+++ ticketing-backend/src/main/java/com/devashree/ticketing/TicketingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devashree.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingBackendApplication.class, args);
+	}
+
+}
